0eb963797d1ebadbf7eb98945a510bea6647d3e0
husam <husambosaeed@gmail.com>
2022-12-26 16:12:15 +0300

    edit

--- JS/main.js
+++ JS/main.js
@@ -4,3 +4,9 @@ let min = time.getMinutes();
 let finished = "انتهى الدوام";
 let working = "متاح";
 let soon = "متبقي";
+
+let finish530 = "";
+let finish1130 = "";
+let finish1 = "";
+let finish10 = "";
+let finish2 = "";
