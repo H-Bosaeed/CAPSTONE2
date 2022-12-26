e93a106b460e25b0f3e7bd1eee2aa4906cb42933
husam <husambosaeed@gmail.com>
2022-12-26 19:06:03 +0300

    js not done

--- JS/main.js
+++ JS/main.js
@@ -4,19 +4,3 @@ let min = time.getMinutes();
 let finished = "انتهى الدوام";
 let working = "متاح";
 let soon = "متبقي";
-
-let finish3 = document.getElementById("red").style.color="green";
-if (hours >= 15 && hours <= 23 || hours == 0) {
-    finish3 = "helloworld";
-}
-if (hours == 14 ) {
-    finish3 = "متبقي";
-    finish3.style.color="#00ff00";
-}
-if (hours < 15 && hours >= 8) {
-    finish3 = "متاح";
-    finish3.style.color="#ff0000";
-}
-let finish10 = "";
-let finish8 = "";
-let finish7 = "";
