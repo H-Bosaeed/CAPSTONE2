37551488ce749432a14ad297a3c6b6b86c0b2ed8
husam <husambosaeed@gmail.com>
2022-12-26 17:41:06 +0300

    confirm

--- JS/main.js
+++ JS/main.js
@@ -5,8 +5,18 @@ let finished = "انتهى الدوام";
 let working = "متاح";
 let soon = "متبقي";
 
-let finish530 = "";
-let finish1130 = "";
-let finish1 = "";
+let finish3 = document.getElementById("red").style.color="green";
+if (hours >= 15 && hours <= 23 || hours == 0) {
+    finish3 = "helloworld";
+}
+if (hours == 14 ) {
+    finish3 = "متبقي";
+    finish3.style.color="#00ff00";
+}
+if (hours < 15 && hours >= 8) {
+    finish3 = "متاح";
+    finish3.style.color="#ff0000";
+}
 let finish10 = "";
-let finish2 = "";
+let finish8 = "";
+let finish7 = "";
