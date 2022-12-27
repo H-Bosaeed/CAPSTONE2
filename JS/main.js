3a579b3f0cd53970059ac8f062c2593b2a845a40
husam <husambosaeed@gmail.com>
2022-12-27 20:24:42 +0300

    reduce Number of lines + edit for large screens

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,5 @@
 let time = new Date ();
-let hours = time.getHours();
+let hours = time.getHours(); /* this function takes the hours as 24 hours method not 12 AM and PM*/
 let min = time.getMinutes();
 let finished = "انتهى الدوام";
 let working = "متاح";
